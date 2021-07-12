baf2b57200f7ef379aee4145dcc01da0393dd1d1
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-07-12 13:18:48 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+set -exo pipefail
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+${THIS_DIR}/CoyoteActors/build.sh
