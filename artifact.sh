a6bd5b9ac4d5847b3c66b45e54bc2e2ea1e06527
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-07-12 22:27:46 +0000

    edits

--- artifact.sh
+++ artifact.sh
@@ -10,7 +10,10 @@ MODE=$1
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 if [ "$MODE" == "build" ]; then
-  bash ${THIS_DIR}/CoyoteActors/artifact.sh build
+  git submodule sync --recursive
+  git submodule update --init --recursive
+  bash ${THIS_DIR}/Nekara/scripts/build.sh
+  # bash ${THIS_DIR}/CoyoteActors/artifact.sh build
 elif [ "$MODE" == "run" ]; then
   bash ${THIS_DIR}/CoyoteActors/artifact.sh run
 else
